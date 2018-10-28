e360ef95ef58670b8d2babb55309f7ce9781177a
akoto101 <lexansaga72@gmail.com>
2018-10-29 02:34:44 +0800

    Commit

---
+++ NotificationDialog/NotificationDialog.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace NotificationDialog
+{
+    partial class NotificationDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationDialog));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.Exitor = new System.Windows.Forms.Timer(this.components);
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.Closer = new System.Windows.Forms.Timer(this.components);
+            this.bunifuFormFadeTransition1 = new Bunifu.Framework.UI.BunifuFormFadeTransition(this.components);
+            this.shower = new System.Windows.Forms.Timer(this.components);
+            this.bunifuImageButton3 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton2 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoEllipsis = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(112, 30);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(260, 25);
+            this.bunifuCustomLabel1.TabIndex = 3;
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoEllipsis = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Roboto", 9.25F);
+            this.bunifuCustomLabel2.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(112, 64);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(260, 54);
+            this.bunifuCustomLabel2.TabIndex = 4;
+            this.bunifuCustomLabel2.Click += new System.EventHandler(this.bunifuCustomLabel2_Click);
+            // 
+            // bunifuCustomLabel3
+            // 
+            this.bunifuCustomLabel3.AutoSize = true;
+            this.bunifuCustomLabel3.Font = new System.Drawing.Font("Roboto Thin", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel3.ForeColor = System.Drawing.Color.White;
+            this.bunifuCustomLabel3.Location = new System.Drawing.Point(336, 128);
+            this.bunifuCustomLabel3.Name = "bunifuCustomLabel3";
+            this.bunifuCustomLabel3.Size = new System.Drawing.Size(60, 18);
+            this.bunifuCustomLabel3.TabIndex = 5;
+            this.bunifuCustomLabel3.Text = "12:00 AM";
+            // 
+            // Exitor
+            // 
+            this.Exitor.Interval = 50;
+            this.Exitor.Tick += new System.EventHandler(this.Exitor_Tick);
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // Closer
+            // 
+            this.Closer.Interval = 5000;
+            this.Closer.Tick += new System.EventHandler(this.Closer_Tick);
+            // 
+            // bunifuFormFadeTransition1
+            // 
+            this.bunifuFormFadeTransition1.Delay = 1;
+            // 
+            // shower
+            // 
+            this.shower.Interval = 50;
+            this.shower.Tick += new System.EventHandler(this.shower_Tick);
+            // 
+            // bunifuImageButton3
+            // 
+            this.bunifuImageButton3.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton3.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton3.Image")));
+            this.bunifuImageButton3.ImageActive = null;
+            this.bunifuImageButton3.Location = new System.Drawing.Point(352, 4);
+            this.bunifuImageButton3.Name = "bunifuImageButton3";
+            this.bunifuImageButton3.Size = new System.Drawing.Size(20, 20);
+            this.bunifuImageButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton3.TabIndex = 2;
+            this.bunifuImageButton3.TabStop = false;
+            this.bunifuImageButton3.Zoom = 10;
+            // 
+            // bunifuImageButton2
+            // 
+            this.bunifuImageButton2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton2.Image = global::NotificationDialog.Properties.Resources.Delete_50px;
+            this.bunifuImageButton2.ImageActive = null;
+            this.bunifuImageButton2.Location = new System.Drawing.Point(376, 4);
+            this.bunifuImageButton2.Name = "bunifuImageButton2";
+            this.bunifuImageButton2.Size = new System.Drawing.Size(20, 20);
+            this.bunifuImageButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton2.TabIndex = 1;
+            this.bunifuImageButton2.TabStop = false;
+            this.bunifuImageButton2.Zoom = 10;
+            this.bunifuImageButton2.Click += new System.EventHandler(this.bunifuImageButton2_Click);
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(16, 26);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(90, 92);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 0;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            // 
+            // NotificationDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(169)))), ((int)(((byte)(242)))));
+            this.ClientSize = new System.Drawing.Size(402, 152);
+            this.Controls.Add(this.bunifuCustomLabel3);
+            this.Controls.Add(this.bunifuCustomLabel2);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.Controls.Add(this.bunifuImageButton3);
+            this.Controls.Add(this.bunifuImageButton2);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NotificationDialog";
+            this.Opacity = 0D;
+            this.Text = "NotificationDialog";
+            this.Load += new System.EventHandler(this.NotificationDialog_Load);
+            this.MouseEnter += new System.EventHandler(this.NotificationDialog_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.NotificationDialog_MouseLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton3;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton2;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private System.Windows.Forms.Timer Exitor;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.Timer Closer;
+        private Bunifu.Framework.UI.BunifuFormFadeTransition bunifuFormFadeTransition1;
+        private System.Windows.Forms.Timer shower;
+    }
+}
---
+++ NotificationDialog/NotificationDialog.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotificationDialog
+{
+    public partial class NotificationDialog : Form
+    {
+        List<string> x = new List<string>();
+        public NotificationDialog()
+        {
+            InitializeComponent();
+        }
+        public NotificationDialog(string title, string message, DialogType type)
+        {
+
+            InitializeComponent();
+            x.Add(@"Res\success.png");
+            x.Add(@"Res\warning.png");
+            x.Add(@"Res\alert.png");
+            x.Add(@"Res\notification.png");
+            bunifuCustomLabel1.Text = title;
+            bunifuCustomLabel2.Text = message;
+            bunifuFormFadeTransition1.ShowAsyc(this);
+            shower.Start();
+            switch (type)
+            {
+                case DialogType.success:
+                    this.BackColor = Color.FromArgb(36, 150, 241);
+                    bunifuImageButton1.Image = Properties.Resources.success;
+                    break;
+                case DialogType.warning:
+                    this.BackColor = Color.FromArgb(255, 129, 4);
+                    bunifuImageButton1.Image = Properties.Resources.warning;
+                    break;
+                case DialogType.alert:
+                    this.BackColor = Color.FromArgb(220, 21, 62);
+                    bunifuImageButton1.Image = Properties.Resources.alert;
+                    break;
+                case DialogType.notify:
+                    this.BackColor = Color.FromArgb(65, 63, 65);
+                    bunifuImageButton1.Image = Properties.Resources.notification;
+                    break;
+
+            }
+        }
+        public static void ShowNotificationDialog(String Title, String Message, DialogType type)
+        {
+            new NotificationDialog(Title, Message, type).Show();
+        }
+
+        public enum DialogType
+        { success, warning, alert, notify }
+        private void bunifuImageButton2_Click(object sender, EventArgs e)
+        {
+            Exitor.Start();
+
+        }
+
+        private void Exitor_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)
+            {
+                this.Opacity -= 0.1;
+            }
+            else
+            {
+                this.Hide();
+            }
+        }
+
+
+
+        private void Closer_Tick(object sender, EventArgs e)
+        {
+            Exitor.Start();
+        }
+
+        private void NotificationDialog_Load(object sender, EventArgs e)
+        {
+            const int margin = 10;
+            int x = Screen.PrimaryScreen.WorkingArea.Right -
+                this.Width - margin;
+            int y = Screen.PrimaryScreen.WorkingArea.Bottom -
+                this.Height - margin;
+            this.Location = new Point(x, y);
+
+            Closer.Start();
+        }
+
+        private void bunifuCustomLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shower_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NotificationDialog_MouseEnter(object sender, EventArgs e)
+        {
+            Closer.Stop();
+        }
+
+        private void NotificationDialog_MouseLeave(object sender, EventArgs e)
+        {
+            Closer.Start();
+        }
+    }
+}
